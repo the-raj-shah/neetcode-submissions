cf76c0e43c9490f60910dc15c9e502538828f50d
Raj Shah <47551436+the-raj-shah@users.noreply.github.com>
2026-06-21 18:46:48 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        outputA = []
+        sortHashSet = {}
+        if len(strs) == 1:
+            return [strs]
+        for eachStr in strs:
+            sortedString = "".join(sorted(eachStr)) 
+            if sortedString in sortHashSet:
+                outputA[sortHashSet[sortedString]].append(eachStr)
+            outputA.append([eachStr])
+            sortHashSet[sortedString] = len(outputA) - 1
+        return outputA
