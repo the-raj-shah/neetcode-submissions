b04708e83e358ef4d0beb94d63af0c82bebbdb24
Raj Shah <47551436+the-raj-shah@users.noreply.github.com>
2026-06-21 18:03:32 +0530

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hashSet = {}
+        if(len(s) != len(t)):
+            return False
+        for c in s:
+            hashSet[c] = hashSet.get(c, 0) + 1
+        for c in t:
+            if c in hashSet:
+                hashSet[c] = hashSet[c] - 1
+                if hashSet[c] == 0:
+                    hashSet.pop(c)
+        return len(hashSet) == 0
