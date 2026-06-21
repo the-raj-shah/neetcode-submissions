2e798a3833914cd9153b5ffa4c62e7458858754f
Raj Shah <47551436+the-raj-shah@users.noreply.github.com>
2026-06-21 17:31:50 +0530

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        unicode = 1.0
+        for c in s:
+            unicode = unicode + (ord(c)- ord('a') + 1)
+        for c in t:
+            unicode = unicode - (ord(c)- ord('a') + 1)
+        return unicode == 1.0
